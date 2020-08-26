11e87a8602cdff8ce8251f15f5efbafc36a882b0
Aryan <aryankumawat33@gmail.com>
2020-08-26 15:15:30 -0400

    Added requirements.txt and getting spreadsheet data

--- api/api.py
+++ api/api.py
@@ -1,8 +1,16 @@
-import time
-from flask import Flask
+import os
+from flask import Flask, jsonify, request, abort
+# Google Sheets API Setup
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 app = Flask(__name__)
 
-@app.route('/time')
-def get_current_time():
-    return {'time': time.time()}
+credential = ServiceAccountCredentials.from_json_keyfile_name("credentials.json",
+                                                              ["https://spreadsheets.google.com/feeds",                                                               "https://www.googleapis.com/auth/spreadsheets",                                                        "https://www.googleapis.com/auth/drive.file",                                                        "https://www.googleapis.com/auth/drive"])
+client = gspread.authorize(credential)
+gsheet = client.open("Certificate Data").sheet1
+
+@app.route('/spreadsheet-data', methods=["GET"])
+def get_spreadsheet_data():
+    return jsonify(gsheet.get_all_records())
