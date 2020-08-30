d7d9f815a999f4d56decfea371985a5729cd8cd6
Aryan <aryankumawat33@gmail.com>
2020-08-30 15:07:37 -0400

    Underlined models

--- src/components/Models.js
+++ src/components/Models.js
@@ -14,7 +14,7 @@ class ModelsPage extends Component {
                             </div>
                             <div class="row">
                                 <div class="col-lg-4">
-                                    <p><h2>After School</h2>
+                                    <p><h2><u>After School</u></h2>
 In this model, the middle school tutors convene at a known location in the evenings around 6:15 pm. Middle school tutors and tutees must find their own transportation to the learning site. Typically, at least 3  volunteers are needed to oversee every 50 students.
 <br/><br/>
 Currently implemented in:<br/>
@@ -24,7 +24,7 @@ Currently implemented in:<br/>
 (Catering to five local elementary schools)</p>
                                 </div>
                                 <div class="col-lg-4">
-                                    <p><h2>Teacher Helper</h2>
+                                    <p><h2><u>Teacher Helper</u></h2>
 As our most popular Model, the Teacher Helper Model allows middle school students to go to elementary schools and assist teachers. Because middle schools typically end at least an hour before elementary schools, tutors are able to join elementary school teachers for the last class period as teacher aides. This model especially supports under served schools or schools without supportive PTA. Transportation is provided through after school or parent volunteers.
 <br/><br/>
 Currently implemented between:<br/>
@@ -32,7 +32,7 @@ Currently implemented between:<br/>
 <i>Herndon Middle and Elementary Schools</i></p>
                                 </div>
                                 <div class="col-lg-4">
-                                    <p><h2>SACC</h2>The SACC Model takes place directly after elementary school as part of the school's School Age Child Care Program. Parent or after school volunteers are needed to transport tutors to the elementary school SACC. <br/><br/> Currently implemented in:<br/>
+                                    <p><h2><u>SACC</u></h2>The SACC Model takes place directly after elementary school as part of the school's School Age Child Care Program. Parent or after school volunteers are needed to transport tutors to the elementary school SACC. <br/><br/> Currently implemented in:<br/>
 
 <i>Brookfield Elementary School</i></p>
                                 </div>
