6d7f14bc565d938d352641784a72a04cbedda071
Aryan <aryankumawat33@gmail.com>
2020-08-30 13:57:51 -0400

    Added image and edited header

--- src/components/Header.js
+++ src/components/Header.js
@@ -14,7 +14,7 @@ class Header extends Component {
                                     <hr class="star-light"/>
                                     <p id="titletext">We are an organization that aims to provide educational assistance to students attending Title 1 schools.</p>
                                     <a href="https://drive.google.com/file/d/16xzAAp-xLIjVT-1qteEF6fEM7vX7x1M7/view" target="_blank" type="button" class="btn rounded-pill btn-lg" id="video">Video!</a>
-                                    <a href="" type="button" class="btn rounded-pill btn-lg" id="getstarted">Get Started</a>
+                                    <a href="/steam-pathways" type="button" class="btn rounded-pill btn-lg" id="getstarted">Check out our new virtual program!</a>
                                 </div>
                             </div>
                     </body>
